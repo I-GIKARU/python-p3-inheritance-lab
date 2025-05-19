555c251f7474ba11d9c756557288c2b25960d863
User <user@example.com>
2025-05-19 13:05:16 +0300

    completed

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        # Initialize knowledge list with at least one item
+        self.knowledge = [
+            "Math", 
+            "Science", 
+            "History", 
+            "Art", 
+            "Literature"
+        ]
 
     def teach(self):
-        pass
+        # Return a random topic from knowledge
+        return random.choice(self.knowledge)
